f18e7c3fb6a2d4d42c428f6399997ac50e4bcee7
jojonki <junki.ohmura@gmail.com>
2019-12-22 14:17:56 +0900

    tiny changes

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 // Listen for messages
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.command && (msg.command == "notify_comment")) {
-        let comment_msg = "<b style='color: darkgreen'>Comments: " + msg.comment + "</b></br />";
-        document.getElementsByClassName('fx metadata')[0].insertAdjacentHTML('afterbegin', comment_msg);
+    if (msg.command) {
+        if (msg.command == "notify_comment") {
+            let comment_msg = "<b style='color: darkgreen'>Comments: " + msg.comment + "</b></br />";
+            document.getElementsByClassName('fx metadata')[0].insertAdjacentHTML('afterbegin', comment_msg);
+        }
     }
 });
