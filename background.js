2868828e582177bb2ad6134f39af889ba40fb3c6
jojonki <junki.ohmura@gmail.com>
2019-12-22 13:50:58 +0900

    clean code

--- background.js
+++ background.js
@@ -10,19 +10,10 @@ function retrieveCommentFromArxiv(arxiv_url, tab) {
             if (comment === null || comment === '') {
                 comment = 'No comments';
             }
-            let options = {
-                type: "basic",
-                iconUrl: "./images/icon_128.png",
-                title: "Author Comments",
-                message: "\"" + comment + "\"",
-            }
             chrome.tabs.sendMessage(tab.id, {
                 command: "notify_comment",
                 comment: comment
             });
-            console.log("call notifications");
-            chrome.notifications.create('copy_with_text_notification', options);
-            // document.getElementById('msg').innerHTML = comment;
         }
     });
 }
@@ -31,26 +22,16 @@ function modifyDOM() {
     return document.body.innerHTML;
 }
 
+// Listen events for url changes
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
-        // read changeInfo data and do something with it
-        // like send the new url to contentscripts.js
         if ('url' in changeInfo) {
-            console.log('ChangeURL:' + changeInfo.url);
             if (changeInfo.url.startsWith('https://feedly.com/i/entry')) {
                 chrome.tabs.executeScript({
-                    code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+                    code: '(' + modifyDOM + ')();'
                 }, (results) => {
                     let $dom = $($.parseHTML(results[0]));
                     let paper_url = $dom.find('div.entryHeader a').attr('href');
-                    // $dom.find('div.fx.metadata').append('<p>#############</p>');
-                    // alert(paper_url);
-                    // alert("--####" + $dom.find('div.entryHeader'));
-                      // chrome.tabs.sendMessage(tab.id, {
-                      //     command: "change_title",
-                      //     title: "hoge"
-                      // });
-                    console.log($dom.find('div.entryHeader'));
                     retrieveCommentFromArxiv(paper_url, tab);
                 });
             }
--- content.js
+++ content.js
@@ -1,21 +1,7 @@
-// // Listen for messages
-// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-//     // If the received message has the expected format...
-//     // if (msg.text === 'report_back') {
-//         // Call the specified callback, passing
-//         // the web-page's DOM content as argument
-//         sendResponse(document.all[0].outerHTML);
-//     // }
-// });
+// Listen for messages
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.command && (msg.command == "notify_comment")) {
-        // var src = document.getElementsByTagName("title")[0].innerHTML;
-        // alert('##########received Response' + msg.comment);
-        // var dst = msg.title;
-        // alert(msg.comment);
         let comment_msg = "<b style='color: darkgreen'>Comments: " + msg.comment + "</b></br />";
         document.getElementsByClassName('fx metadata')[0].insertAdjacentHTML('afterbegin', comment_msg);
-        // document.getElementsByTagName("title")[0].innerHTML = dst;
-        // sendResponse("the page title's changed: '" + src + "' -> '" + dst + "'");
     }
 });
